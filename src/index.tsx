68dc6c1affa3126aa23d9fde0b7c982552625632
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-07 15:07:37 +0200

    add define property

--- src/index.tsx
+++ src/index.tsx
@@ -29,10 +29,14 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": "'production'",
+        global: "window",
+      },
     })
 
-    console.log(result)
+    // console.log(result)
 
     setCode(result.outputFiles[0].text)
   }
